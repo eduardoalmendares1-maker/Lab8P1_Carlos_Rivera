490b5d988a8f1c20bb4903d4e19c29e63795262d
Carlos Rivera <eduardo.almendares1@icloud.com>
2026-06-12 16:23:27 -0600

    EX1C

--- src/lab8p1_carlos_rivera/Lab8P1_Carlos_Rivera.java
+++ src/lab8p1_carlos_rivera/Lab8P1_Carlos_Rivera.java
@@ -25,26 +25,45 @@ public class Lab8P1_Carlos_Rivera {
         }
         return C;
     }
-    public static void union(int[] A,int[] B){
-        for(int i=0;i<A.length;i++){
-            System.out.print(A[i]+" ");
+
+    public static void union(int[] A, int[] B) {
+        for (int i = 0; i < A.length; i++) {
+            System.out.print(A[i] + " ");
         }
-        for(int i=0;i<B.length;i++){
-            boolean encontrado=false;
-            for(int j=0;j<A.length;j++){
-                if(A[i]==B[i]){
-                    encontrado=true;
+        for (int i = 0; i < B.length; i++) {
+            boolean encontrado = false;
+            for (int j = 0; j < A.length; j++) {
+                if (A[j] == B[i]) {
+                    encontrado = true;
                     break;
                 }
             }
-            if(!encontrado){
-                System.out.print(B[i]+" ");
-                
+            if (!encontrado) {
+                System.out.print(B[i] + " ");
+
             }
         }
         System.out.println();
     }
+    public static void inter(int[] A2,int[] B2){
+    boolean encontrados=false;
+    for(int i=0;i<A2.length;i++){
+        for(int j=0;j< B2.length;j++){
+            if(A2[i]==B2[j]){
+                System.out.println("INTERSECCION:"+A2[i]);
+                encontrados=true;
+                break;
+            }
+            
+        }
+    }
+    if(!encontrados){
+        System.out.println("NO HAY NUMERO EN COMUN");
+        
+    }
     
+}
+
     public static void main(String[] args) {
         int opcion;
         do {
@@ -71,22 +90,32 @@ public class Lab8P1_Carlos_Rivera {
                             System.out.println("1. UNION");
                             e.nextLine();
                             System.out.print("TAMANIO DEL CONJUNTO A:");
-                            int arre1=e.nextInt();
-                            int[] A=llenar(arre1);
+                            int arre1 = e.nextInt();
+                            int[] A = llenar(arre1);
                             System.out.print("TAMANIO DEL CONJUNTO B:");
-                            int arre2=e.nextInt();
-                            int[] B=llenar(arre2);
-                            union(A,B);
-                            
-                            
-                            
+                            int arre2 = e.nextInt();
+                            int[] B = llenar(arre2);
+                            union(A, B);
+
                             break;
                         case 2:
-                            System.out.println("2. SUPERMERCADO ");
+                            System.out.println("2. INTERSECCION");
                             e.nextLine();
-                                                       
+                            System.out.print("TAMANO DEL CONJUNTO A:");
+                            int arr1=e.nextInt();
+                            int[] A2=llenar(arr1);
+                            System.out.print("TAMANO DEL CONJUNTO B:");
+                            int arr2=e.nextInt();
+                            int[] B2=llenar(arr2);
+                            inter(A2,B2);
+                            
                             break;
                         case 3:
+                            System.out.println("3. DIFERENCIA A - B");
+                            e.nextLine();
+                            System.out.println("TAMANIO DEL CONJUNTO A:");
+                            int ar1=e.nextInt();
+                            
                             break;
                         case 4:
                             break;
