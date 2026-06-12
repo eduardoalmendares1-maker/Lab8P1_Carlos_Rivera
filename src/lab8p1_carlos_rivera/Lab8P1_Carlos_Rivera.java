12db7608544a362eddb5f8870f7699d220435602
Carlos Rivera <eduardo.almendares1@icloud.com>
2026-06-12 13:20:07 -0600

    Lab8P1_Carlos_Rivera

---
+++ src/lab8p1_carlos_rivera/Lab8P1_Carlos_Rivera.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab8p1_carlos_rivera;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Lab8P1_Carlos_Rivera {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
